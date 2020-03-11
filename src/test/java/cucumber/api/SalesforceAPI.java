32ed662773ecbdd0152a0a3b37ff25e600c23469
Yurii Zosin <yuriizosin@bpp.com>
2020-03-11 16:18:14 +0200

    edited API tests

--- src/test/java/cucumber/api/SalesforceAPI.java
+++ src/test/java/cucumber/api/SalesforceAPI.java
@@ -1,12 +1,13 @@
 package cucumber.api;
 
 import api.requests.SalesforceBusinessProcesses;
+import api.requests.SalesforceBusinessProcessesUAT;
 import io.cucumber.java.en.*;
 
 public class SalesforceAPI {
     @When("I create new faculty record")
     public void i_create_new_faculty_record() {
-       new SalesforceBusinessProcesses().createNewFaculty();
+        new SalesforceBusinessProcesses().createNewFaculty();
     }
 
     @Then("I should submit faculty")
@@ -16,13 +17,22 @@ public class SalesforceAPI {
 
     @And("I create new student account")
     public void i_should_create_student_account() {
-        new SalesforceBusinessProcesses().
-                createNewStudent().
-                addAddress().
-                getAccountProfileId().
-                addOpportunity().
-                addOffering().
-                submitNewAccount();
+        if (System.getProperties().containsKey("environment")&& System.getProperty("environment").equals("UAT")) {
+            new SalesforceBusinessProcessesUAT().
+                    createNewStudentUAT().
+                    addAddressUAT().
+                    getAccountProfileIdUAT().
+                    addOpportunityUAT().
+                    addOfferingUAT().
+                    submitNewAccountUAT();
+        } else {
+            new SalesforceBusinessProcesses().
+                    createNewStudent().
+                    addAddress().
+                    getAccountProfileId().
+                    addOpportunity().
+                    addOffering().
+                    submitNewAccount();
+        }
     }
-
 }
