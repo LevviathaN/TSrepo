74fdef69c06d0ddc38fbcfb326ac21f1f088ed22
Yurii Zosin <yuriizosin@bpp.com>
2020-05-20 12:53:11 +0300

    merged dev

--- src/main/java/ui/utils/DriverProvider.java
+++ src/main/java/ui/utils/DriverProvider.java
@@ -152,7 +152,6 @@ public class DriverProvider {
             options.setCapability("browserstack.console", "errors");
             options.setCapability("browserstack.localIdentifier", "TestAutomation");
 
-            //options.setCapability("browserstack.timeouts", "{\"implicit\"=>0, \"pageLoad\"=>60000, \"script\"=>60000}");
             options.setCapability(ChromeOptions.CAPABILITY, options);
             options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
             options.setCapability(ChromeOptions.CAPABILITY, options);
