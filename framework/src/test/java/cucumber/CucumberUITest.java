969599388e72cab2442f14b742942dafe46bbeb7
ruslan.levytskyi <Castiel619>
2021-03-25 13:14:55 +0200

    Unmuted CBAs.

--- framework/src/test/java/cucumber/CucumberUITest.java
+++ framework/src/test/java/cucumber/CucumberUITest.java
@@ -15,7 +15,7 @@ import ui.utils.RetryAnalyzer;
 @CucumberOptions(
         features = "src/test/resources/cucumber/bpp_features",
         glue = {"cucumber.stepdefs"},
-        tags = {"@ProductFactory and not @BlockedByIssue and not @DoNotRun and not @DataGeneration and not @CBA"},
+        tags = {"@ProductFactory and not @BlockedByIssue and not @DoNotRun and not @DataGeneration"},
         plugin = {"pretty"})
 
 public class CucumberUITest extends BaseUITest {
