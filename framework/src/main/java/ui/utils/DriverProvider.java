f28ff09467a06e5a740228441ce1caf3b25704fe
ruslan.levytskyi <Castiel619>
2021-06-08 17:28:27 +0300

    Timeouts revert

--- framework/src/main/java/ui/utils/DriverProvider.java
+++ framework/src/main/java/ui/utils/DriverProvider.java
@@ -140,7 +140,7 @@ public class DriverProvider {
             options.setCapability("browserstack.local", "true");
             options.setCapability("browserstack.console", "errors");
             options.setCapability("browserstack.localIdentifier", "TestAutomation");
-            options.setCapability("browserstack.idleTimeout", "180");
+            options.setCapability("browserstack.idleTimeout", "280");
             options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
             options.setCapability(ChromeOptions.CAPABILITY, options);
             options.setExperimentalOption("prefs", chromePreferences);
